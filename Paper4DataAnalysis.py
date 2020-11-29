5bc7c16842477292fafb8ebb8ec5e8ed938ed158
mouna2 <mounahammoudi2@gmail.com>
2020-11-29 05:01:56 +0100

    First commit

--- Paper4DataAnalysis.py
+++ Paper4DataAnalysis.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import matplotlib as matplotlib
 
-Frequency = [15097,1207,645,93,68,15,19,10,20,3,4,3,1,1,1,1,1,1,2]
-variableAmount = ['0', '1', '2', '3', '4', '5', '6', '7', '8','10', '12','13','14', '15', '20', '23', '24', '26', '30'] #sample names
+Frequency = [14775,1726,464,105,62,12,11,7,13,4,2,2,4,1,1,1,2]
+variableAmount = ['0', '1', '2', '3', '4', '5', '6', '7', '8','9', '10','12','13', '16', '20', '21', '25'] #sample names
 
 
 variableAmount_int=[int(x) for x in variableAmount]
@@ -15,6 +16,24 @@ plt.xlabel('Variables per method')
 plt.ylabel('Frequency')
 plt.show()
 
+
+###########################################
+
+print('matplotlib: {}'.format(matplotlib.__version__))
+
+variableAmount = [16,20,21,10,12,25,9,13,7,6,5,8,4,3,2,1,0]
+Frequency = ['1','1','1','2','2','2','4','4','7','11','12','13','62','105','464','1726','14775'] #sample names
+helper = np.arange(len(Frequency))
+
+plt.bar(helper, variableAmount)
+plt.xticks(ticks=helper, labels=Frequency, rotation=45)
+
+plt.xlabel('Number of Methods')
+plt.ylabel('Variables')
+plt.show()
+###########################################
+###########################################
+###########################################
 ###########################################
 
 dataset = pd.read_csv( 'TNUPercentages.txt', sep= ',', index_col=False) 
